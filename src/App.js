701318a5fe9fcc6ea16583071d06bc43c41ceb61
yull0222 <bonheurdoux22@gmail.com>
2025-03-13 22:33:03 +0900

    .환경설정정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { GrFavorite } from "react-icons/gr";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-slate-500'>
           Edit <code>src/App.js</code> and save to reload.
+          <GrFavorite />
         </p>
         <a
           className="App-link"
